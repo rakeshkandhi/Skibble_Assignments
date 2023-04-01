f9021fe7f6e09782d060500e9762a78538bbbbd6
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-04-01 23:04:04 +0530

    init

--- Exercise_01/main.js
+++ Exercise_01/main.js
@@ -1,5 +1,5 @@
-const backGround = document.getElementsByTagName('body');
+const backGround = document.getElementsByTagName("body");
 function changeBackgroundColor(event) {
-    var myColor = event.target.value;
-    backGround[0].style.backgroundColor = myColor;
+  var myColor = event.target.value;
+  backGround[0].style.backgroundColor = myColor;
 }
--- Exercise_03/main.js
+++ Exercise_03/main.js
@@ -1,3 +1,24 @@
-function add(){
-    // num1 = document.;
+function add() {
+  x = parseInt(document.frmadd.txt1.value);
+  y = parseInt(document.frmadd.txt2.value);
+  result = x + y;
+  document.frmadd.txt3.value = result;
+}
+function sub() {
+  x = parseInt(document.frmadd.txt1.value);
+  y = parseInt(document.frmadd.txt2.value);
+  result = x - y;
+  document.frmadd.txt3.value = result;
+}
+function division() {
+  x = parseInt(document.frmadd.txt1.value);
+  y = parseInt(document.frmadd.txt2.value);
+  result = x / y;
+  document.frmadd.txt3.value = result;
+}
+function multiply() {
+  x = parseInt(document.frmadd.txt1.value);
+  y = parseInt(document.frmadd.txt2.value);
+  result = x * y;
+  document.frmadd.txt3.value = result;
 }
